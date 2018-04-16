882d59cba21c3310379553301f8c3510207d75f7
kokushkin <fipsiksi@yandex.ru>
2018-04-16 21:16:28 +0300

    3_2_1 и 3_2_2

---
+++ BaseAspNet/3_2_1/WebForm1.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _3_2_1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Выполнить инициализацию, только если страница запрашивается впервые. 
+            // После этого данная информация отслеживается в состоянии представления
+            if (!IsPostBack)
+            {
+                // Установить атрибуты стиля для настройки внешнего вида страницы
+                TextBox1.Style["font-size"] = "20px";
+                TextBox1.Style["color"] = "red";
+
+                // Использовать немного отличающийся, но эквивалентный 
+                // синтаксис для установки атрибута стиля
+                TextBox1.Style.Add("background-color", "lightyellow");
+
+                // Установить текст, отображаемый по умолчанию
+                TextBox1.Value = "<Вставьте ваш e-mail>";
+
+                // Установить другие нестандартные атрибуты. 
+                TextBox1.Attributes["onfocus"] = "alert(TextBox1.value);";
+
+            }
+        }
+    }
+}
---
+++ BaseAspNet/3_2_2/WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace _3_2_2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Создать новый объект HtmlTable
+            HtmlTable table1 = new HtmlTable();
+
+            // Установить свойства форматирования таблицы
+            table1.Border = 1;
+            table1.CellPadding = 3;
+            table1.CellSpacing = 3;
+            table1.BorderColor = "red";
+
+            // Начать добавлять содержимое в таблицу
+            HtmlTableRow row;
+            HtmlTableCell cell;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                // Создать новую строку и установить для нее цвет фона
+                row = new HtmlTableRow();
+                row.BgColor = (i % 2 == 0 ? "lightyellow" : "lightcyan");
+
+                for (int j = 1; j <= 4; j++)
+                {
+                    // Создать ячейку и установить ее текст
+                    cell = new HtmlTableCell();
+                    cell.InnerHtml = "Row: " + i.ToString() +
+                        "<br />Cell: " + j.ToString();
+
+                    // Добавить ячейку в текущую строку
+                    row.Cells.Add(cell);
+                }
+
+                // Добавить строку в таблицу
+                table1.Rows.Add(row);
+            }
+
+            // Добавить таблицу внутрь страницы
+            this.Controls.Add(table1);
+
+        }
+    }
+}
