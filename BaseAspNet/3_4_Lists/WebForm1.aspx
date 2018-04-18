5c1e4318f35f76c949ed021beae65c7d8df136c3
kokushkin <fipsiksi@yandex.ru>
2018-04-18 08:58:54 +0300

    3_4_Lists

---
+++ BaseAspNet/3_4_Lists/WebForm1.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_3_4_Lists.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ListBox runat="server" ID="Listbox1" SelectionMode="Multiple" Rows="5">
+                <asp:ListItem Selected="True">Option1</asp:ListItem>
+                <asp:ListItem>Option2</asp:ListItem>
+            </asp:ListBox>
+            <br />
+            <br />
+            <asp:DropDownList runat="server" ID="DropdownList1">
+                <asp:ListItem Selected="True">Option 1</asp:ListItem>
+                <asp:ListItem>Option 2</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            <asp:CheckBoxList runat="server" ID="CheckboxList1" RepeatColumns="3">
+                <asp:ListItem Selected="true">Option 1</asp:ListItem>
+                <asp:ListItem>Option 2</asp:ListItem>
+            </asp:CheckBoxList>
+            <br />
+            <asp:RadioButtonList runat="server" ID="RadiobuttonList1"
+                RepeatDirection="Horizontal" RepeatColumns="2">
+                <asp:ListItem Selected="true">Option 1</asp:ListItem>
+                <asp:ListItem>Option 2</asp:ListItem>
+            </asp:RadioButtonList>
+
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ BaseAspNet/3_4_Lists/WebForm1.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _3_4_Lists
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                for (int i = 3; i <= 5; i++)
+                {
+                    Listbox1.Items.Add("Option " + i.ToString());
+                    DropdownList1.Items.Add("Option " + i.ToString());
+                    CheckboxList1.Items.Add("Option " + i.ToString());
+                    RadiobuttonList1.Items.Add("Option " + i.ToString());
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, System.EventArgs e)
+        {
+            Response.Write("<b>Выбранные элементы в Listbox1:</b><br/>");
+            foreach (ListItem li in Listbox1.Items)
+            {
+                if (li.Selected) Response.Write("- " + li.Text + "<br/>");
+            }
+
+            Response.Write("<b>Выбранный элемент в DropdownList1:</b><br/>");
+            Response.Write("- " + DropdownList1.SelectedItem.Text + "<br/>");
+
+            Response.Write("<b>Выбранные элементы в CheckboxList1:</b><br/>");
+            foreach (ListItem li in CheckboxList1.Items)
+            {
+                if (li.Selected) Response.Write("- " + li.Text + "<br/>");
+            }
+
+            Response.Write("<b>Выбранный элемент в RadiobuttonList1:</b><br/>");
+            Response.Write("- " + RadiobuttonList1.SelectedItem.Text + "<br/><br/><br/>");
+        }
+    }
+}
