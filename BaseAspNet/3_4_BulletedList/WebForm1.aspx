a2537e2e2b89c4852e9efba58070d395ca727815
kokushkin <fipsiksi@yandex.ru>
2018-04-18 22:02:11 +0300

    3_4_BulletedList

---
+++ BaseAspNet/3_4_BulletedList/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_3_4_BulletedList.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+            Bullet styles:<br />
+            <br />
+            <asp:BulletedList BulletStyle="Numbered" DisplayMode="LinkButton" ID="BulletedList1"
+                OnClick="BulletedList1_Click" runat="server">
+            </asp:BulletedList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ BaseAspNet/3_4_BulletedList/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _3_4_BulletedList
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                foreach (string style in Enum.GetNames(typeof(BulletStyle)))
+                {
+                    BulletedList1.Items.Add(style);
+                }
+            }
+        }
+
+        protected void BulletedList1_Click(object sender, BulletedListEventArgs e)
+        {
+            string styleName = BulletedList1.Items[e.Index].Text;
+            BulletStyle style = (BulletStyle)Enum.Parse(typeof(BulletStyle), styleName);
+            BulletedList1.BulletStyle = style;
+        }
+    }
+}
