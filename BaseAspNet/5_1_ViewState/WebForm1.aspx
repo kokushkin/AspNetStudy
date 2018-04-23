4ddb02ca4eb14cece1decdfca890b069900b4973
kokushkin <fipsiksi@yandex.ru>
2018-04-23 16:52:07 +0300

    эксперименты с viewState. Хотя я и отключил viewState, состояние элементов сохранаятся!

---
+++ BaseAspNet/5_1_ViewState/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_5_1_ViewState.WebForm1"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+<form id="form1" runat="server">    
+<div>
+   <table>
+      <tr>
+         <td>Имя:</td>
+         <td><asp:TextBox runat="server" Width="200px" ID="Name" EnableViewState="false"/></td>
+      </tr>
+      <tr>
+         <td>ID:</td>
+         <td><asp:TextBox runat="server" Width="200px" ID="EmpID" EnableViewState="false"/></td>
+      </tr>
+      <tr>
+         <td>Возраст:</td>
+         <td><asp:TextBox runat="server" Width="200px" ID="Age" EnableViewState="false"/></td>
+      </tr>
+      <tr>
+         <td>E-mail:</td>
+         <td><asp:TextBox runat="server" Width="200px" ID="Email" EnableViewState="false"/></td>
+      </tr>
+      <tr>
+         <td>Пароль:</td>
+         <td><asp:TextBox TextMode="Password" runat="server" Width="200px" ID="Password" /></td>
+      </tr>
+   </table>
+   <br /><asp:Button runat="server" Text="Save" ID="cmdSave" OnClick="cmdSave_Click" />
+    <asp:Button ID="cmdRestore" runat="server" Text="Restore" OnClick="cmdRestore_Click"></asp:Button><br />
+</div></form>
+</body>
+</html>
---
+++ BaseAspNet/5_1_ViewState/WebForm1.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _5_1_ViewState
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void cmdSave_Click(object sender, EventArgs e)
+        {
+            return;
+            // Сохранить текущий текст        
+            SaveAllText(Page.Controls, true);
+        }
+
+        private void SaveAllText(ControlCollection controls, bool saveNested)
+        {
+            foreach (Control control in controls)
+            {
+                if (control is TextBox)
+                {
+                    // Сохранить текст с использованием уникального                 
+                    // идентификатора элемента управления                
+                    ViewState[control.ID] = ((TextBox)control).Text;
+                }
+
+                if ((control.Controls != null) && saveNested)
+                {
+                    SaveAllText(control.Controls, true);
+                }
+            }
+        }
+
+        private void RestoreAllText(ControlCollection controls, bool saveNested)
+        {
+            foreach (Control control in controls)
+            {
+                if (control is TextBox)
+                {
+                    if (ViewState[control.ID] != null)
+                        ((TextBox)control).Text = (string)ViewState[control.ID];
+                }
+
+                if ((control.Controls != null) && saveNested)
+                {
+                    RestoreAllText(control.Controls, true);
+                }
+            }
+        }
+
+        protected void cmdRestore_Click(object sender, EventArgs e)
+        {
+            // Извлечь последний сохраненный текст        
+            RestoreAllText(Page.Controls, true);
+        }
+    }
+}
