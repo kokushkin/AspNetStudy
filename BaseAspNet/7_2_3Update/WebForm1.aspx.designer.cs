061d0a6d8e58f048f0fb8899e2eb79b55086fc85
kokushkin <fipsiksi@yandex.ru>
2018-05-08 01:15:28 +0300

    DetailsView to add elements

--- BaseAspNet/7_2_3Update/WebForm1.aspx.designer.cs
+++ BaseAspNet/7_2_3Update/WebForm1.aspx.designer.cs
@@ -38,5 +38,23 @@ namespace _7_2_3Update {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// DetailsView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DetailsView DetailsView1;
+        
+        /// <summary>
+        /// FormView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FormView FormView1;
     }
 }
