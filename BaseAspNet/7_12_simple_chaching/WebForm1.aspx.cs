986f000ebb7d6d64f7820d68acc4cb1f9d50a5e2
kokushkin <fipsiksi@yandex.ru>
2018-05-21 02:13:05 +0300

    7_12_simple_chaching

---
+++ BaseAspNet/7_12_simple_chaching/WebForm1.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _7_12_simple_chaching
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = "<h1>Сейчас: <br>" + DateTime.Now.ToString() + "</h1>";
+        }
+    }    
+}
