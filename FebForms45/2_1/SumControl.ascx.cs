15717d2cf4def0d0ea1fec13b3602e292a79c8c8
kokushkin <fipsiksi@yandex.ru>
2018-09-06 10:05:33 +0300

    webforms_4_5 2_1

---
+++ FebForms45/2_1/SumControl.ascx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ErrorHandling
+{
+    public partial class SumControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                int? first = Int32.Parse(Request.Form["first"]);
+                int? second = Int32.Parse(Request.Form["second"]);
+                result.InnerText = (first + second).ToString();
+                resultPlaceholder.Visible = true;
+            }
+        }
+    }
+}
