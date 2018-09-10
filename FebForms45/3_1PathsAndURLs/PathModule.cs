5383484bedd3c157ea96ca0c4be422b9477e0263
kokushkin <fipsiksi@yandex.ru>
2018-09-10 10:41:14 +0300

    WebForms45_3_2

---
+++ FebForms45/3_1PathsAndURLs/PathModule.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace PathsAndURLs
+{
+    public class PathModule : IHttpModule
+    {
+        private static readonly string[] extensions = { ".aspx", ".ashx" };
+        public void Init(HttpApplication app)
+        {
+            app.BeginRequest += (src, args) => HandleRequest(app);
+        }
+
+        private void HandleRequest(HttpApplication app)
+        {
+            if (app.Request.CurrentExecutionFilePathExtension == String.Empty)
+            {
+                string target = null;
+                string vpath = app.Request.CurrentExecutionFilePath;
+
+                if (vpath == "/")
+                {
+                    target = "/Default.aspx";
+                }
+                else
+                {
+                    foreach (string ext in extensions)
+                    {
+                        if (File.Exists(app.Request.MapPath(vpath + ext)))
+                        {
+                            target = vpath + ext;
+                            break;
+                        }
+                    }
+                }
+
+                if (target != null)
+                {
+                    app.Context.RewritePath(target);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            // Ничего не освобождается
+        }
+    }
+}
