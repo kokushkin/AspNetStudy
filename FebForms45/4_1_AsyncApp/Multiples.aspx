4ed74114135b949eb7ff44d54b5eb6c8b67cbf2c
kokushkin <fipsiksi@yandex.ru>
2018-09-11 22:41:25 +0300

    WebForms45_4_2_before_tpl

---
+++ FebForms45/4_1_AsyncApp/Multiples.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Multiples.aspx.cs" Inherits="AsyncApp.Multiples" 
+    Async="true"%>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+    <title></title>
+    <style>
+        table { border: thin solid black; border-collapse: collapse; font-family: Tahoma}
+        th, td { text-align: left; padding: 5px; border: thin solid black;}
+    </style>
+</head>
+<body>
+    <table>
+        <tr><th>Время начала</th><th>URL</th><th>Длина</th></tr>
+        <asp:Repeater id="rep" SelectMethod="GetResults" 
+                ItemType="AsyncApp.MultiWebSiteResult" runat="server">
+            <ItemTemplate>
+                <tr>
+                    <td><%# Item.StartTime %></td>
+                    <td><%# Item.Url %></td>
+                    <td><%# Item.Length %></td>
+                </tr>
+            </ItemTemplate>
+        </asp:Repeater>
+    </table>
+</body>
+</html>
---
+++ FebForms45/4_1_AsyncApp/Multiples.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AsyncApp
+{
+    public class MultiWebSiteResult
+    {
+        public string Url { get; set; }
+        public long Length { get; set; }
+        public long StartTime { get; set; }
+    }
+
+    public partial class Multiples : System.Web.UI.Page
+    {
+        private ConcurrentQueue<MultiWebSiteResult> results;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] targetUrls
+                = { "http://asp.net", "http://professorweb.ru", "http://google.com" };
+            results = new ConcurrentQueue<MultiWebSiteResult>();
+
+            foreach (string targetUrl in targetUrls)
+            {
+                MultiWebSiteResult result = new MultiWebSiteResult { Url = targetUrl };
+                results.Enqueue(result);
+
+                RegisterAsyncTask(new PageAsyncTask(async () =>
+                {
+                    result.StartTime
+                            = (long)DateTime.Now.Subtract(Context.Timestamp).TotalMilliseconds;
+                    string webContent = await new WebClient().DownloadStringTaskAsync(targetUrl);
+                    result.Length = webContent.Length;
+                    rep.DataBind();
+                }));
+            }
+        }
+
+        public IEnumerable<MultiWebSiteResult> GetResults()
+        {
+            return results;
+        }
+    }
+}
