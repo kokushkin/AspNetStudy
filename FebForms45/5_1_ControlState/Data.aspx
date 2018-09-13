7bf40a21aa33a8a4c97a870cf15dac76bcabf49e
kokushkin <fipsiksi@yandex.ru>
2018-09-13 06:32:11 +0300

    WebForms45_5_3_OldFashion

---
+++ FebForms45/5_1_ControlState/Data.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Data.aspx.cs" Inherits="ControlState.Data" %>
+<%@ Register TagPrefix="CC" Assembly="5_1_ControlState" Namespace="ControlState.Controls" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <input id="max" value="5" runat="server" />
+            <CC:OperationSelector id="opSelector" runat="server" />
+            <button type="submit">Выполнить</button>
+        </div>
+        <div>
+            <asp:Repeater SelectMethod="GetData" ItemType="System.String" 
+                    runat="server" ViewStateMode="Disabled">
+                <ItemTemplate>
+                    <p><%# Item %></p>
+                </ItemTemplate>
+            </asp:Repeater>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FebForms45/5_1_ControlState/Data.aspx.cs
@@ -0,0 +1,41 @@
+﻿using ControlState.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ControlState
+{
+    public partial class Data : System.Web.UI.Page
+    {
+        int maxValue;
+        string operation;
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                maxValue = int.Parse(max.Value);
+                OperationSelector selector = FindControl("opSelector") as OperationSelector;
+
+                if (selector != null)
+                    operation = selector.SelectedOperator;
+            }
+        }
+
+        public IEnumerable<string> GetData()
+        {
+            if (operation != null)
+            {
+                for (int i = 1; i < maxValue; i++)
+                {
+                    yield return string.Format("{0} {1} {2} = {3}",
+                        maxValue, operation == "Сложить" ? "+" : "-",
+                        i, operation == "Сложить" ? (maxValue + i) : (maxValue - i));
+                }
+            }
+        }
+    }
+}
