0e38f2310f14d3247314596ead4ee2c90e788a4e
kokushkin <fipsiksi@yandex.ru>
2018-09-19 15:19:42 +0300

    AspNetMVC_1_5

---
+++ AspNetMVC/WebApplicaion2/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicaion2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            int firstVal = 10;
+            int secondVal = 0;
+            int result = firstVal / secondVal;
+
+            //ViewBag.Message = "Отладка приложения ASP.NET MVC!";
+
+            return View(result);
+        }
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
