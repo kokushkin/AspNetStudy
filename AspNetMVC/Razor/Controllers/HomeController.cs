4bce1e42dd9dc03ae7508b9781b32b7937d8ffa6
kokushkin <fipsiksi@yandex.ru>
2018-09-20 00:36:00 +0300

    AspNetMVC_2_1

---
+++ AspNetMVC/Razor/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Каяк",
+            Description = "Лодка на одного человека",
+            Category = "Водные виды спорта",
+            Price = 275M
+        };
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+    }
+}
