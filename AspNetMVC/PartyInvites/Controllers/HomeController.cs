b7c1ea764c7e3128b4a4fd4bf168580598601d1c
kokushkin <fipsiksi@yandex.ru>
2018-09-19 08:20:29 +0300

    AspNetMVC_1_4 before "Стилизация содержимого"

--- AspNetMVC/PartyInvites/Controllers/HomeController.cs
+++ AspNetMVC/PartyInvites/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartyInvites.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,24 @@ namespace PartyInvites.Controllers
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Доброго дня";
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guest)
+        {
+            if (ModelState.IsValid)
+                // Нужно отправить данные нового гостя по электронной почте 
+                // организатору вечеринки.
+                return View("Thanks", guest);
+            else
+                // Обнаружена ошибка проверки достоверности
+                return View();
         }
     }
 }
