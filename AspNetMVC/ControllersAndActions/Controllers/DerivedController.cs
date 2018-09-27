897ea1197bb212518c2a46e9c415e2d0b6816fc3
Kulikov Nikolay <fipsiksi@yandex.ru>
2018-09-27 12:13:04 +0300

    AspNetMVC_4_1

---
+++ AspNetMVC/ControllersAndActions/Controllers/DerivedController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class DerivedController : Controller
+    {
+        // GET: Derived
+        public ActionResult Index()
+        {
+            ViewBag.Message =
+                "Привет из контроллера DerivedController метода действия Index";
+            return View("MyView");
+        }
+    }
+}
