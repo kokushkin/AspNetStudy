bfa9b18cb7ddb1dbf46434abdf5000e75b4d6c59
Kulikov Nikolay <fipsiksi@yandex.ru>
2018-09-28 00:25:47 +0300

    AspNetMVC_4_4

---
+++ AspNetMVC/ControllersAndActions.Tests/ActionTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web.Mvc;
+using ControllersAndActions.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControllersAndActions.Tests
+{
+    [TestClass]
+    public class ActionTests
+    {
+        [TestMethod]
+        public void ControllerTest()
+        {
+            // Организация - создание контроллера
+            ExampleController controller = new ExampleController();
+
+            // Действие - вызов метода действия
+            ViewResult result = controller.Index();
+
+            // Утверждение - проверка результата
+            Assert.AreEqual("", result.ViewName);
+            Assert.AreEqual("Привет", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void ViewSelectionTest()
+        {
+            // Организация - создание контроллера
+            ExampleController controller = new ExampleController();
+
+            // Действие - вызов метода действия
+            ViewResult result = controller.Index();
+
+            // Утверждение - проверка результата
+            Assert.AreEqual("", result.ViewName);
+            Assert.IsInstanceOfType(result.ViewData.Model, typeof(System.DateTime));
+        }
+    }
+}
---
+++ AspNetMVC/ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // GET: Example
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Привет";
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
+    }
+}
