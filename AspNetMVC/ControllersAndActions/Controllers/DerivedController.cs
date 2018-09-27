c7d562c43ff064823275ef3eb7c351e95baab099
Kulikov Nikolay <fipsiksi@yandex.ru>
2018-09-27 12:35:03 +0300

    AspNetMVC_4_2

--- AspNetMVC/ControllersAndActions/Controllers/DerivedController.cs
+++ AspNetMVC/ControllersAndActions/Controllers/DerivedController.cs
@@ -15,5 +15,29 @@ namespace ControllersAndActions.Controllers
                 "Привет из контроллера DerivedController метода действия Index";
             return View("MyView");
         }
+
+        public ActionResult ActionMethod()
+        {
+            // Получить доступ к разнообразным свойствам из объектов контекста
+            string userName = User.Identity.Name;
+            string serverName = Server.MachineName;
+            string clientIP = Request.UserHostAddress;
+            DateTime dateStamp = HttpContext.Timestamp;
+
+            // Извлечь отправленные данные из Request.Form 
+            string oldProductName = Request.Form["OldName"];
+            string newProductName = Request.Form["NewName"];
+
+            // ...
+
+            return View();
+        }
+
+        public ActionResult WeatherForecast(DateTime forDate, string city = "Москва", int page = 1)
+        {
+            // реализовать прогноз погоды
+            return View();
+        }
+
     }
 }
