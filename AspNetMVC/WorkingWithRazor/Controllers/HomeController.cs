a8afd5f904b803de609308f09c978fab9c0326e8
Kulikov Nikolay <fipsiksi@yandex.ru>
2018-09-29 21:13:11 +0300

    AspNetMVC_4_11

---
+++ AspNetMVC/WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkingWithRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string[] names = { "Яблоко", "Апельсин", "Груша" };
+            return View(names);
+        }
+
+        public ViewResult List()
+        {
+            return View();
+        }
+    }
+}
