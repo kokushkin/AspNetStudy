9ab2bfa872f8912bcef00b536178faf3e31b7b59
Kulikov Nikolay <fipsiksi@yandex.ru>
2018-10-01 22:37:35 +0300

    AspNetMVC_5_2

--- AspNetMVC/Filter/Controllers/HomeController.cs
+++ AspNetMVC/Filter/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Filter.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace Filter.Controllers
 {
     public class HomeController : Controller
     {
+        //[Authorize(Users = "admin")]
+        [CustomAuth(true)]
         public string Index()
         {
             return "Это метод действия Index в контроллере Home";
