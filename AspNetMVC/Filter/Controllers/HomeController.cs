9a022252972bddb28e6b409860917c8345307dac
Kulikov Nikolay <fipsiksi@yandex.ru>
2018-10-01 20:12:47 +0300

    AspNetMVC_5_1 not complited

---
+++ AspNetMVC/Filter/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Это метод действия Index в контроллере Home";
+        }
+    }
+}
