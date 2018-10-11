8d1ed8d0e5c182d69433f360566c25a56a53d890
kokushkin <fipsiksi@yandex.ru>
2018-10-11 09:26:34 +0300

    GameStore_2_2  before Отображение ссылок на страницы

--- GameStoreMVC/GameStore.UnitTests/UnitTest1.cs
+++ GameStoreMVC/GameStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using GameStore.WebUI.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace GameStore.UnitTests
 {
@@ -7,8 +13,29 @@ namespace GameStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            // Организация (arrange)
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game { GameId = 1, Name = "Игра1"},
+                new Game { GameId = 2, Name = "Игра2"},
+                new Game { GameId = 3, Name = "Игра3"},
+                new Game { GameId = 4, Name = "Игра4"},
+                new Game { GameId = 5, Name = "Игра5"}
+            });
+            GameController controller = new GameController(mock.Object);
+            controller.pageSize = 3;
+
+            // Действие (act)
+            IEnumerable<Game> result = (IEnumerable<Game>)controller.List(2).Model;
+
+            // Утверждение (assert)
+            List<Game> games = result.ToList();
+            Assert.IsTrue(games.Count == 2);
+            Assert.AreEqual(games[0].Name, "Игра4");
+            Assert.AreEqual(games[1].Name, "Игра5");
         }
     }
 }
