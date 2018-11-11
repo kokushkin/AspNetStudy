980a81fbf9278f0d4f34c71476d643b0f4d1239e
kokushkin <fipsiksi@yandex.ru>
2018-11-11 10:19:42 +0200

    Identity_1_1

---
+++ AspNetIdentity/Users/Models/AppUserModels.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Users.Models
+{
+    public class AppUser : IdentityUser
+    {
+        // Здесь будут указываться дополнительные свойства
+    }
+}
