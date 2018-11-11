cb8f65446e801f83698f048eb92638b67bb2ba17
kokushkin <fipsiksi@yandex.ru>
2018-11-11 23:15:11 +0200

    Identity_1_3

--- AspNetIdentity/Users/Infrastructure/AppUserManager.cs
+++ AspNetIdentity/Users/Infrastructure/AppUserManager.cs
@@ -21,6 +21,19 @@ namespace Users.Infrastructure
         {
             AppIdentityDbContext db = context.Get<AppIdentityDbContext>();
             AppUserManager manager = new AppUserManager(new UserStore<AppUser>(db));
+
+            manager.PasswordValidator = new CustomPasswordValidator
+            {
+                RequiredLength = 6,
+                RequireNonLetterOrDigit = false,
+                RequireDigit = false,
+                RequireLowercase = true,
+                RequireUppercase = true
+            };
+
+            manager.UserValidator = new CustomUserValidator();
+
+
             return manager;
         }
     }
