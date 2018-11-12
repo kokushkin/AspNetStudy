50fd9bc07200e17683d9322f3150321418d0ee4a
kokushkin <fipsiksi@yandex.ru>
2018-11-12 19:31:35 +0200

    Identity_1_7

--- AspNetIdentity/Users/Infrastructure/AppIdentityDbContext.cs
+++ AspNetIdentity/Users/Infrastructure/AppIdentityDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -33,6 +34,32 @@ namespace Users.Infrastructure
         public void PerformInitialSetup(AppIdentityDbContext context)
         {
             // настройки конфигурации контекста будут указываться здесь
+
+            AppUserManager userMgr = new AppUserManager(new UserStore<AppUser>(context));
+            AppRoleManager roleMgr = new AppRoleManager(new RoleStore<AppRole>(context));
+
+            string roleName = "Administrators";
+            string userName = "Admin";
+            string password = "mypassword";
+            string email = "admin@professorweb.ru";
+
+            if (!roleMgr.RoleExists(roleName))
+            {
+                roleMgr.Create(new AppRole(roleName));
+            }
+
+            AppUser user = userMgr.FindByName(userName);
+            if (user == null)
+            {
+                userMgr.Create(new AppUser { UserName = userName, Email = email },
+                    password);
+                user = userMgr.FindByName(userName);
+            }
+
+            if (!userMgr.IsInRole(user.Id, roleName))
+            {
+                userMgr.AddToRole(user.Id, roleName);
+            }
         }
     }
 }
