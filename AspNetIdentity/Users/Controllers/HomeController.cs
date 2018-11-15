e9c933117abca5cea157160cd71b8be7309f71b6
Kulikov Nikolay <fipsiksi@yandex.ru>
2018-11-15 23:09:34 +0200

    Identity_1_8 not complited

--- AspNetIdentity/Users/Controllers/HomeController.cs
+++ AspNetIdentity/Users/Controllers/HomeController.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Users.Infrastructure;
+using Users.Models;
 
 namespace Users.Controllers
 {
@@ -35,6 +42,56 @@ namespace Users.Controllers
             dict.Add("В роли Users?", HttpContext.User.IsInRole("Users"));
 
             return dict;
+        }
+
+        [Authorize]
+        public ActionResult UserProps()
+        {
+            return View(CurrentUser);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult> UserProps(Cities city)
+        {
+            AppUser user = CurrentUser;
+            user.City = city;
+            await UserManager.UpdateAsync(user);
+            return View(user);
+        }
+
+        private AppUser CurrentUser
+        {
+            get
+            {
+                return UserManager.FindByName(HttpContext.User.Identity.Name);
+            }
+        }
+
+        private AppUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            }
+        }
+
+        // Вспомогательный метод, загружающий название элемента перечисления
+        // из атрибута Display
+        [NonAction]
+        public static string GetCityName<TEnum>(TEnum item)
+            where TEnum : struct, IConvertible
+        {
+            if (!typeof(TEnum).IsEnum)
+            {
+                throw new ArgumentException("Тип TEnum должен быть перечислением");
+            }
+            else
+                return item.GetType()
+                    .GetMember(item.ToString())
+                    .First()
+                    .GetCustomAttribute<DisplayAttribute>()
+                    .Name;
         }
     }
 }
--- AspNetIdentity/Users/Models/AppUserModels.cs
+++ AspNetIdentity/Users/Models/AppUserModels.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Users.Models
 {
+    public enum Cities
+    {
+        [Display(Name = "Лондон")]
+        LONDON,
+
+        [Display(Name = "Париж")]
+        PARIS,
+
+        [Display(Name = "Москва")]
+        MOSCOW
+    }
+
     public class AppUser : IdentityUser
     {
-        // Здесь будут указываться дополнительные свойства
+        public Cities City { get; set; }
     }
 }
